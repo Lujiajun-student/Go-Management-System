fdaaf1c92378777e76b4b87d651ef7b82cc8eb3a
Lujiajun-student <johnmactavish745@gmail.com>
2026-03-27 12:40:58 +0800

    实现登录逻辑

--- admin-vue/src/api/index.js
+++ admin-vue/src/api/index.js
@@ -5,10 +5,19 @@
 import request from '@/utils/request'
 
 export default {
+    // 验证码接口
     captcha() {
         return request ({
             url: '/captcha',
             method: 'get'
+        })
+    },
+    // 登录接口
+    login(params) {
+        return request({
+            url: '/login',
+            method: 'post',
+            data: params
         })
     }
 }
--- admin-vue/src/views/Login.vue
+++ admin-vue/src/views/Login.vue
@@ -2,27 +2,27 @@
 <template>
   <div class="login_container">
     <div class="login_box">
-      <el-form class="login_form">
+      <el-form class="login_form" ref="loginFormRef" :rules="rules" :model="loginForm">
         <div class="title">
           通用后台管理系统
         </div>
         <el-form-item prop="username">
-          <el-input placeholder="账号" prefix-icon="el-icon-user-solid"></el-input>
+          <el-input placeholder="账号" prefix-icon="el-icon-user-solid" v-model="loginForm.username" clearable></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input placeholder="密码" prefix-icon="el-icon-key"></el-input>
+          <el-input placeholder="密码" prefix-icon="el-icon-key" v-model="loginForm.password" clearable></el-input>
         </el-form-item>
-        <el-form-item prop="验证码">
-          <el-input placeholder="验证码" prefix-icon="el-icon-view" style="width: 200px; float: left; " maxlength="6"/>
-          <el-image class="captchaImg" style="width: 150px; float: left;" :src="image" @click="getCaptcha()"/>
+        <el-form-item prop="image">
+          <el-input placeholder="验证码" prefix-icon="el-icon-view" style="width: 200px; float: left; " maxlength="6" v-model="loginForm.image" clearable/>
+          <el-image class="captchaImg" style="width: 150px; float: left;" :src="image" @click="getCaptcha"/>
         </el-form-item>
         <el-form-item>
           <el-row :gutter="20">
             <el-col :span="12" :offset="0">
-              <el-button type="primary" style="width: 100%; font-size: large;">登录</el-button>
+              <el-button type="primary" style="width: 100%; font-size: large;" @click="loginBtn">登录</el-button>
             </el-col>
             <el-col :span="12" :offset="0">
-              <el-button type="info" style="width: 100%; font-size: large;">重置</el-button>
+              <el-button type="info" style="width: 100%; font-size: large;" @click="resetForm">重置</el-button>
             </el-col>
           </el-row>
         </el-form-item>
@@ -37,6 +37,29 @@ export default {
   data() {
     return {
       image: '',
+      rules: {
+        username: [
+          {
+            required: true, message:"请输入账号", trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true, message:"请输入密码", trigger: "blur"
+          }
+        ],
+        image: [
+          {
+            required: true, message:"请输入验证码", trigger: "blur"
+          }
+        ]
+      },
+      loginForm: {
+        username: '',
+        password: '',
+        image: '',
+        id_key: ''
+      }
     }
   },
   methods: {
@@ -47,9 +70,34 @@ export default {
         this.$message.error(res.message)
       }else {
         this.image = res.data.image
+        // 封装验证码id
+        this.loginForm.id_key = res.data.idKey
       }
+    },
+    // 重置表单
+    resetForm() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 登录
+    loginBtn() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log("传输参数：", this.loginForm)
+        if (valid) {
+          const {data: res} = await this.$api.login(this.loginForm)
+          // console.log("获取登录的res数据：", res)
+          if (res.code !== 200) {
+            this.$message.error(res.message)
+          } else {
+            this.$message.success("登录成功")
+            this.$router.push("/home")
+          }
+        } else {
+          return false
+        }
+      })
     }
   },
+
   created() {
     this.getCaptcha()
   }
